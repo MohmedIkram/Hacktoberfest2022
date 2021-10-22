d8b0083869cf58370545d23d1e484ea261405b15
Shivam Bhalla <shivambhalla1499@gmail.com>
2021-10-22 18:34:04 +0530

    Add files via upload

---
+++ slot-machine-game/src/Matching.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Matching = () =>{
+    return ( 
+        <>       
+        <div className ="slot_inner">
+        <h1>This is matching</h1>
+        <hr/>
+        </div>
+        </>
+    )
+}
+
+export default Matching;
