af649e43eddac6e8aeafa77705672911d543b367
Shivam Bhalla <shivambhalla1499@gmail.com>
2021-10-22 18:44:14 +0530

    Slot-machine-game
    Slot-machine-game

---
+++ React/slot-machine-game/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SlotM from './SlotM';
+
+const App =()=>{
+    return <>
+
+ <h1 className="slotmachine"> 🎰 Welcome to Slot machine game🎰</h1>
+    
+        <div className="slotmachine">
+
+    <SlotM x='😃' y='🎅'z='😃'/>
+    <SlotM x='😃' y='😃' z='😃'/>git 
+    <SlotM x='😃' y='😃' z='😃'/>
+
+    </div>
+    </>
+   
+}
+
+export default App;
