100b00c8e91a4e129c952f9d7020d8d7a656b36b
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2021-10-24 08:43:11 +0530

    Added JavaScript Monster Killer Game Folder
    . This folder consists of index.html which you can open and run the game
    . assets folder has JavaScript and CSS files
    . In this game you can choose you health and same health will be given to monster
    . You have  four options "ATTACK", "STRONG ATTACK", "HEAL" and "SHOW LOG"
    . On clicking ATTACK random value of attack will be done by you to monster and vice versa.
    . On clicking STRONG ATTACK your attack will be increased by some value so that monster can get high damage
    . HEAL option can increase your health to a little rando value
    . SHOW LOG button will show full log of all buttons and attacks in Console if you want to see how much value of ATTACK, STRONG ATTACK, HEAL was there at time of playing.
    . That set enjoy the game.

---
+++ javascript Games/Monster-Killer-Game/assets/scripts/vendor.js
@@ -0,0 +1,44 @@
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
+
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-attack-btn');
+const healBtn = document.getElementById('heal-btn');
+const logBtn = document.getElementById('log-btn');
+
+function adjustHealthBars(maxLife) {
+  monsterHealthBar.max = maxLife;
+  monsterHealthBar.value = maxLife;
+  playerHealthBar.max = maxLife;
+  playerHealthBar.value = maxLife;
+}
+
+function dealMonsterDamage(damage) {
+  const dealtDamage = Math.random() * damage;
+  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+  return dealtDamage;
+}
+
+function dealPlayerDamage(damage) {
+  const dealtDamage = Math.random() * damage;
+  playerHealthBar.value = +playerHealthBar.value - dealtDamage;
+  return dealtDamage;
+}
+
+function increasePlayerHealth(healValue) {
+  playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value) {
+  playerHealthBar.value = value;
+  monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function setPlayerHealth(health) {
+  playerHealthBar.value = health;
+}
