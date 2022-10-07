a8a0d89f0316330d72c9b6d60f74c1c2dbf8b09a
Priyanshu <priyanshupuri0@gmail.com>
2022-10-07 12:36:02 +0530

    C++ program for sorting is added.

---
+++ C++/Sorting Algorithm/shellSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    for (int gap = size / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < size; i++)
+        {
+            int temp = arr[i];
+            int j;
+
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                arr[j] = arr[j - gap];
+
+            arr[j] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int a[] = {2, 1, 5, 3, 4, 7, 6, 0};
+    int size = sizeof(a) / sizeof(a[0]);
+    sort(a, size);
+    printArray(a, size);
+}
