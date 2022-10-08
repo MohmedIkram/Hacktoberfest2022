1b1ea5c21e568e88b2735eff097bb2cac3108c0e
MohmedIkram <mohmedikram123@gmail.com>
2022-10-08 11:02:53 -0700

    update

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -1,40 +1,28 @@
-public class BubbleSortExample 
-{
-    public static void main(String[] args) 
-    {
-        // This is unsorted array
-        Integer[] array = new Integer[] { 12, 13, 24, 10, 3, 6, 90, 70 };
- 
-        // Let's sort using bubble sort
-        bubbleSort(array, 0, array.length);
- 
-        // Verify sorted array
-        System.out.println(Arrays.toString(array));
-    }
- 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static void bubbleSort(Object[] array, int fromIndex, int toIndex) 
-    {
-        Object d;
-        for (int i = toIndex - 1; i > fromIndex; i--) 
-        {
-            boolean isSorted = true;
-            for (int j = fromIndex; j < i; j++) 
-            {
-                //If elements in wrong order then swap them
-                if (((Comparable) array[j]).compareTo(array[j + 1]) > 0) 
-                {
-                    isSorted = false;
-                    d = array[j + 1];
-                    array[j + 1] = array[j];
-                    array[j] = d;
-                }
-            }
-            //If no swapping then array is already sorted
-            if (isSorted)
-                break;
-        }
-    }
+public static void main(String[] args)
+ {
+
+  int number[]={7,13,4,5,62,3,1,3,45};
+
+  int temp;
+  boolean fixed=false;
+  while(fixed==false){
+      fixed = true;
+
+  for (int i=0; i <number.length-1;i++){ 
+      if (number[i]>number[i+1]){
+
+      temp = number [i+1];
+
+      number[i+1]=number[i];
+
+      number[i]=temp;
+      fixed=false;
+      }
+ }
 }
- 
-Output: [3, 6, 10, 12, 13, 24, 70, 90]
+  for (int i=0; i<number.length;i++){
+      System.out.println(number[i]);
+   }
+  }
+
+}
