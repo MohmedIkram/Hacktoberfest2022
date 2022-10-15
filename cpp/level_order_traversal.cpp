8a41f1f8726af92901665769c04eaf48fa3e0cc2
Tusshar Paul <96624900+tussharpaul13@users.noreply.github.com>
2022-10-15 21:36:00 +0530

    Tree Programs.cpp
    Programs in C++
    1. To find height of binary tree
    2, To find postorder traversal of binary tree
    3. To find preorder traversal of binary tree
    4. To find levelorder traversal of binary tree

---
+++ cpp/level_order_traversal.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+void printLevel(Node *root){
+    if(root==NULL)return;
+    queue<Node *>q;
+    q.push(root);
+    while(q.empty()==false){
+        Node *curr=q.front();
+        q.pop();
+        cout<<curr->key<<" ";
+        if(curr->left!=NULL)
+            q.push(curr->left);
+        if(curr->right!=NULL)
+            q.push(curr->right);
+    }
+}  
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	root->right->right=new Node(70);
+	
+	printLevel(root);
+}
