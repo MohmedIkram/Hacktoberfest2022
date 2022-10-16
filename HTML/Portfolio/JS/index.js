9595ef1af8bfc4e7b70df5d62b281ddc14e207d9
Thanistas <thanithanis171198@gmail.com>
2022-10-16 21:43:56 +0530

    Add portfolio template

---
+++ HTML/Portfolio/JS/index.js
@@ -0,0 +1,46 @@
+
+
+
+function initParadoxWay() {
+    "use strict";
+   
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+                
+            }
+        });
+    }
+    
+    
+}
+
+$(document).ready(function () {
+    initParadoxWay();
+});
+
+
+//message suceess
+
+function contact() {
+    alert("Your message successfully send !");
+  }
