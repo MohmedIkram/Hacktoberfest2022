a9570e7ac22c92cecc72d659bffc58deaa09fd88
Tannistha Biswas <tannisthabiswasofficial@gmail.com>
2022-10-19 02:22:11 +0530

    Added new array program in java

---
+++ Java/Kadane'salgorithm.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+    }
+}
+/*Very important algotithm to find subarray with maximum sum. 
+ * Level: medium
+ * Time complexity : o(NxM)  
+  Space Complexity o(1)*/
