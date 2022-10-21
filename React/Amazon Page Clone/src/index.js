839546bc74f46743e1d30e6d8a9129534bde7940
Erdnernie <48798916+adiahn@users.noreply.github.com>
2022-10-20 23:25:25 -0700

    Re-indented the arragements

--- React/Amazon Page Clone/src/index.js
+++ React/Amazon Page Clone/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><App /></React.StrictMode>, 
   document.getElementById('root')
 );
