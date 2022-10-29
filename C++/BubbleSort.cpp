be658d8bb19eabdfabc0b6e1d0b7c8b1e088c4c9
Hiza Tariq <bsef19m505@pucit.edu.pk>
2022-10-29 09:37:30 +0500

    Added a csharp program

--- C++/BubbleSort.cpp
+++ C++/BubbleSort.cpp
@@ -1,37 +1,37 @@
-// C++ program for implementation
-// of Bubble sort
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-	int i, j;
-	for (i = 0; i < n - 1; i++)
+int main(){
 
-		// Last i elements are already
-		// in place
-		for (j = 0; j < n - i - 1; j++)
-			if (arr[j] > arr[j + 1])
-				swap(arr[j], arr[j + 1]);
-}
+    int n;
+    cin>>n;
+    int arry[n];
 
-// Function to print an array
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-// Driver code
-int main()
-{
-	int arr[] = { 5, 1, 4, 2, 8};
-	int N = sizeof(arr) / sizeof(arr[0]);
-	bubbleSort(arr, N);
-	cout << "Sorted array: \n";
-	printArray(arr, N);
-	return 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arry[i];
+    }
+    
+    int counter=1;
+    while (counter<n)
+    {
+        for (int i = 0; i < n-counter; i++)
+        {
+            if (arry[i]>arry[i+1])
+            {
+                int temp=arry[i];
+                arry[i]=arry[i+1];
+                arry[i+1]=temp;
+            }
+            
+        }
+        counter++;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arry[i]<<" ";
+    }
+    return 0;
+    
 }
